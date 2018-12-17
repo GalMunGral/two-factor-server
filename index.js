8333019e6c8848ad399458f21baebdf66c546e9e
Wenqi He <hewenqi96@gmail.com>
2018-12-17 04:00:24 -0500

    Add TCP server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+const net = require('net');
+
+// Start TCP server
+const server = net.createServer((socket) => {
+  socket.write('Hello\n');
+})
+server.listen(2000, '127.0.0.1', () => {
+  console.log('Listening on 2000');
+});
