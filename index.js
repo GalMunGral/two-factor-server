46353ff845ff90443377eb4f1178a3d90e55a40c
Wenqi He <hewenqi96@gmail.com>
2018-12-24 03:14:09 -0500

    Authorize for calling FCM server

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const net = require('net');
+// const net = require('net');
+const { google } = require('googleapis');
+const key = require('./service-account.json');
+
+const jwtClient = new google.auth.JWT(
+  key.client_email,
+  null, // keyFile
+  key.private_key,
+  ['https://www.googleapis.com/auth/firebase.messaging'], // scopes
+  null,
+);
+
+jwtClient.authorize((err, token) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('token:', token);
+});
+
 
 // Start TCP server
-const server = net.createServer((socket) => {
-  socket.write(new Date().toString());
-})
-server.listen(3000, () => {
-  console.log('Listening on 3000');
-});
+// const server = net.createServer((socket) => {
+//   socket.write(new Date().toString());
+// })
+// server.listen(3000, () => {
+//   console.log('Listening on 3000');
+// });
